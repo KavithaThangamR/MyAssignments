4dd2019bcb1b8eb971e5ac12191c9e30690e2a3d
KavithaThangamR <kavithasivathanu15@gmail.com>
2024-10-16 00:19:07 +0530

    day2

---
+++ LearnIfelse.java
@@ -0,0 +1,16 @@
+package day2.week1;
+
+public class LearnIfelse {
+
+	public static void main(String[] args) {
+		int day = 5;
+		
+		if (day > 0) 
+			System.out.println("positive");
+		else
+			System.out.println("negative");
+		
+
+	}
+
+}
