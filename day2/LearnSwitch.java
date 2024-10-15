ce2752e743c2ee77a06255b690823cb62376f86e
KavithaThangamR <kavithasivathanu15@gmail.com>
2024-10-16 00:30:59 +0530

    day2

---
+++ day2/LearnSwitch.java
@@ -0,0 +1,33 @@
+package day2.week1;
+
+public class LearnSwitch {
+
+	public static void main(String[] args) {
+		int day=5;
+		
+		switch (day) {
+		case 1:
+			System.out.println("monday");
+			break;
+		case 2:
+			System.out.println("tuesday");
+			break;
+		case 3:
+			System.out.println("wednesday");
+			break;
+		case 4:
+			System.out.println("thursday");
+			break;
+		case 5:
+			System.out.println("friday");
+			break;
+			default:
+				System.out.println("invalid");
+				break;
+				
+				
+		}
+
+	}
+
+}
