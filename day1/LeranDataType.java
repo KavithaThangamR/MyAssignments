516346173a9a6c956ab70b4005457e342d547c49
KavithaThangamR <kavithasivathanu15@gmail.com>
2024-10-15 23:53:34 +0530

    Add files via upload
    day1

---
+++ day1/LeranDataType.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class LeranDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int regNo=1345;
+		String colour = "Black";
+		char logo='S';
+		boolean type=true;
+		float peed=8.15f;
+		double capacity=1478.9834678;
+		long phoneNo=345678903;
+		short big=13456;
+		System.out.println(type);
+		
+		
+
+	}
+
+}
